d7d0c72fc6407206e6381d03d98c013b5b9f2648
nafisnihal <nafisnihal12@gmail.com>
2023-05-19 17:50:38 +0600

    logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import lofo from "../assets/images/Neural Logo.png";
+import logo from "../assets/images/Neurallogo.png";
 
 const Header = () => {
   return (
     <div className="bg-dark">
-      <img src={lofo} alt="logo" className="ms-4 my-3" />
+      <img src={logo} alt="logo" className="ms-4 my-3" />
     </div>
   );
 };
