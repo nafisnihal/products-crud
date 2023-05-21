3f30ec6a132ffc5adca51c32dd7ce618a1d33470
nafisnihal <nafisnihal12@gmail.com>
2023-05-21 18:17:41 +0600

    addproductmodal

--- src/Components/Action.jsx
+++ src/Components/Action.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Action.scss";
+import AddProduct from "./AddProduct";
 
 const Action = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div id="action">
-      <button className="add-btn">Add Inventory</button>
+      <button className="add-btn" onClick={handleShow}>
+        Add Inventory
+      </button>
       <input
         type="search"
         name=""
@@ -12,6 +20,7 @@ const Action = () => {
         className="search-btn"
         placeholder="Search here"
       />
+      {show && <AddProduct show={show} handleClose={handleClose} />}
     </div>
   );
 };
